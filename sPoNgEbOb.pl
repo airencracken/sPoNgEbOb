ec9801d58ae41891879eea4144d7a42c1c376997
Marcus J. Hildum <marcus.hildum@gmail.com>
2019-12-26 15:56:32 -0800

    needs toilet, but more amusing to me

--- sPoNgEbOb.pl
+++ sPoNgEbOb.pl
@@ -2,36 +2,8 @@
 
 use strict;
 use warnings;
-use feature 'say';
 
-my $string = join "", @ARGV;
-my $normalized;
+my $string = join "", map { rand(100) < 50 ? "\u$_":"\l$_" } split "", (join "", <STDIN>);
 
-my $rand = int(rand(10));
+print `toilet -f wideterm $string`;
 
-if ($rand % 2 == 0) {
-	$normalized = lc $string;
-} else {
-	$normalized = uc $string;
-}
-
-my @stringarray = split "", $normalized;
-my @mocking;
-
-for (my $i = 0; $i <= (length($normalized) - 1); $i++) {
-	if ($i % 2 == 0) {
-		push @mocking, $stringarray[$i];
-		next;
-	}
-	if ($stringarray[$i] =~ m/[A-Z]/) {
-		my $new;
-		$new = lc $stringarray[$i];
-		push @mocking, $new;
-	} else {
-		my $new;
-		$new = uc $stringarray[$i];
-		push @mocking, $new;
-	}
-}
-
-say join "", @mocking;
