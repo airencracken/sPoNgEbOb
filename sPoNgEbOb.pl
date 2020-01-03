2754185e4811108abea43db00b056dfc0cdc34bf
Marcus J. Hildum <marcus.hildum@gmail.com>
2020-01-03 01:17:50 -0800

    quotes 'n shit

--- sPoNgEbOb.pl
+++ sPoNgEbOb.pl
@@ -5,5 +5,5 @@ use warnings;
 
 my $string = join "", map { rand(100) < 50 ? "\u$_":"\l$_" } split "", (join "", <STDIN>);
 
-print `toilet -f wideterm -w 1024 $string`;
+print `toilet -f wideterm -w 1024 "$string"`;
 
